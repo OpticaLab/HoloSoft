13a5da2c6d374d4df5755831c20648d20738f3fb
claudriel <31657600+claudriel@users.noreply.github.com>
2024-02-06 13:01:50 +0100

    Update ImgCorrect.py

--- analysis/ImgCorrect.py
+++ analysis/ImgCorrect.py
@@ -18,80 +18,93 @@ import argparse
 Parser for run all the folds. Class args for one fold.
 
 """
-parser = argparse.ArgumentParser("Programma Correzione Immagini")
+parser = argparse.ArgumentParser("Correct Image")
 parser.add_argument('-fd','--folder_dir', required=True, type=str, help="Folder Directory")
 parser.add_argument('-sd','--stack_dir', required=True, type=str, help="Working Stack Directory")
 parser.add_argument('-sv', '--save_img', action='store_true', help="Save correct images" )
 parser.add_argument('-del', '--delete_img', action='store_true', help="Delete medians" )
 parser.add_argument('-st', '--std_dev', required=True, type=float, help="Standard deviation cut off")
+parser.add_argument('-stp', '--step', required=True, type=int, help="Step of average")
 
 args = parser.parse_args()
 
+S = True
+N = False
+
 # class args:
-#     folder_dir = "RICE/1"
-#     stack_dir = "1"
-#     save_img = True
-#     delete_img = True
-#     std_dev = 0.0138
+#     folder_dir = ""
+#     stack_dir = ""
+#     save_img = 
+#     delete_img = 
+#     std_dev =
+#     step = 
 
 
-sample = args.folder_dir + "/" + args.stack_dir+"/dati/"
-path_list = os.listdir(sample)
+for m in args.stack_dir[:]:
+    print(m)
+    sample = args.folder_dir + "/" + str(args.stack_dir[m])+"/data/"
+    path_list = os.listdir(sample)
 
-img_list = os.listdir(sample)
-img_list.sort()
+    img_list = os.listdir(sample)
+    img_list.sort()
 
-directory_save_bg = args.folder_dir + "/" + args.stack_dir+"/mediana"
-directory_save_correct = args.folder_dir + "/" + args.stack_dir+"/img_correct"
+    directory_save_bg = args.folder_dir + "/" + str(args.stack_dir[m])+"/bg"
+    directory_save_correct = args.folder_dir + "/" + str(args.stack_dir[m])+"/img_correct"
 
-try:
-    os.stat(directory_save_bg)
-except:
-    os.makedirs(directory_save_bg)
-
-if args.save_img:
     try:
-        os.stat(directory_save_correct)
+        os.stat(directory_save_bg)
     except:
-        os.makedirs(directory_save_correct)
+        os.makedirs(directory_save_bg)
 
+    if args.save_img:
+        try:
+            os.stat(directory_save_correct)
+        except:
+            os.makedirs(directory_save_correct)
 
-start = 0
-end = 20
-std_array = np.array([])
-
-for i in np.arange(0, int(len(path_list)/end), 1):
-    I_array = []
-
-    for k in range(start, end):
-        I_array.append(Image.open(sample + img_list[k]).convert("L"))
-    I_array = np.array([np.asarray(im) for im in I_array])
-    img_median = np.median(I_array, axis=0)
-    result = Image.fromarray(img_median.astype('uint8'))
-    result.save(directory_save_bg+'/median_' +
-                format(start, "04")+'_'+format(end, "04")+'.tiff')
-
+    start = 0
+    end = args.step
     std_array = np.array([])
-    for j in range(start, end):
-        im = Image.open(sample + img_list[j]).convert("L")
-        I = np.asarray(im)
-        I_correct = (I)/(img_median)
+
+    for i in np.arange(0, len(path_list)/args.step, 1):
+        print(start)
+        I_array = []
+        for k in range(start, end):
+            
+            I_array.append(Image.open(sample + img_list[k]).convert("L"))
+        I_array = np.array([np.asarray(im) for im in I_array])
+        img_median = np.median(I_array, axis=0)
+        result = Image.fromarray(img_median.astype('uint8'))
+        result.save(directory_save_bg+'/median_' + format(start, "04")+'_'+format(end, "04")+'.tiff')
+        std_array = np.array([])
         
-        st = np.std(I_correct)
-        std_array = np.append(std_array, st)
+        for j in range(start, end):
+            im = Image.open(sample + img_list[j]).convert("L")
+            I = np.asarray(im)
+            I_correct = (I)/(img_median)
+            st = np.std(I_correct)
+            std_array = np.append(std_array, st)
+            
+            if args.delete_img:
+                if st < args.std_dev:
+                    print("canceled",img_list[j])
+                    os.remove(sample + img_list[j])
 
-        if args.save_img:
-            I_correct = I_correct/np.amax(I_correct)*255
-            result = Image.fromarray(I_correct.astype('uint8'))
-            result.save(directory_save_correct + '/img_' + str(j)+'.tiff')
+           
+            if args.save_img:
+                plt.imshow(I_correct, cmap = "viridis")
+                plt.savefig(directory_save_correct + '/img_' + str(j)+'_'+str(st)+'.png')
+                plt.clf()
+                plt.close()
+                # I_correct = I_correct/np.amax(I_correct)*255
+                # result = Image.fromarray(I_correct.astype('uint8'))
+                # result.save(directory_save_correct + '/img_' + str(j)+'_'+str(st)+'.tiff')
 
-    if args.delete_img:
-        if all(k < args.std_dev for k in std_array):
-            for m in range(start, end):
-                os.remove(sample + img_list[m])
-            os.remove(directory_save_bg+'/median_' +
-                      format(start, "04")+'_'+format(end, "04")+'.tiff')
-            print("la media è stata cancellata")
-
-    start = start + 20
-    end = end + 20
+        if args.delete_img:
+            if all(k < args.std_dev for k in std_array):
+                print("canceled", '/median_' + format(start, "04")+'_'+format(end, "04"))
+                os.remove(directory_save_bg+'/median_' + format(start, "04")+'_'+format(end, "04")+'.tiff')
+                
+        start = start + args.step
+        end = end + args.step
+        
